c6d9ca2802d136330af6c6bd41e6ddea532b1850
Christopher Bebry <invalidsyntax@gmail.com>
2013-05-22 04:25:31 +0000

    Added lobby file.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var crunch = require('./lib');
 var async = require('async');
 //TODO: async this properly
-;
+
 //initialize system
 console.log("Bootstrapping system setup.")
 crunch.bootstrap(function() {
 	console.log("System bootstrap complete.");
+	console.log("System entering lobby mode.")
+	crunch.lobby(function() {
+		console.log("Lobby is up.");
+	}
 });
 
 //console.log("System entering lobby mode.");
